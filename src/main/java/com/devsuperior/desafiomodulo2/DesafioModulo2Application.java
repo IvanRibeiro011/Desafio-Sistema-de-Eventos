c591ec7e226b228c0700e30fa045d85bee414e20
Ivanildo Ribeiro <Ivanribeiro011@hotmail.com>
2023-05-16 16:19:32 -0300

    Desafio concluido

---
+++ src/main/java/com/devsuperior/desafiomodulo2/DesafioModulo2Application.java
@@ -0,0 +1,13 @@
+package com.devsuperior.desafiomodulo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioModulo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioModulo2Application.class, args);
+	}
+
+}
